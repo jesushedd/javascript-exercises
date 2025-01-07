d7a235fea80eaa2d00e822597cef586c9cf02acd
jesushedd <jesushe99@gmail.com>
2025-01-07 01:23:07 -0500

    Solve exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,40 @@
-const add = function() {
+const add = function(a,b) {
+  return a + b;
 	
 };
 
-const subtract = function() {
+const subtract = function(a,b) {
+  return a - b;
 	
 };
 
-const sum = function() {
+const sum = function(arr) {
+  let sum = 0;
+  for (n of arr){
+    sum += n
+  }
+  return sum;
 	
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let mult = 1;
+  for (n of arr){
+    mult *= n
+  }
+  return mult
 };
 
-const power = function() {
+const power = function(a,b) {
+  return Math.pow(a,b);
 	
 };
 
-const factorial = function() {
+const factorial = function(a) {
+  if (a === 1 || a === 0){
+    return 1
+  }
+  return a * factorial(a-1);
 	
 };
 
