abdb7241d1ca372535c6d056efd3700dd95429ff
jesushedd <jesushe99@gmail.com>
2025-01-07 00:57:14 -0500

    solve exercise 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,9 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    let divBy4 =  (Math.round(year % 4,0) === 0)
+    let divBy100 =  (Math.round(year % 100,0) === 0)
+    let divBy400 = (Math.round(year % 400,0) === 0)
+    
+    return (divBy4 && !divBy100) || divBy400
 
 };
 
