aa87fbe83be526cd225b4bab51c8b46124973477
jesushedd <jesushe99@gmail.com>
2025-01-06 23:59:18 -0500

    Add exercise 4 solution

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr) {
+    let toRemove = Array.prototype.slice.call(arguments,1)
+
+    
+
+    for (str of toRemove){
+        arr = arr.filter(s => s !== str)
+    }
+    return arr
 };
 
 // Do not edit below this line
